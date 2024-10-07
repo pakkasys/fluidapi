9394706a377addb0144f7eb4cdb4a64e797b7424
Aatu Harju <aatu.harju@gmail.com>
2024-10-07 10:03:50 +0300

    Definition merging

--- endpoint/definition/definition.go
+++ endpoint/definition/definition.go
@@ -1,9 +1,91 @@
 package definition
 
-import "github.com/pakkasys/fluidapi/endpoint/middleware"
+import (
+	"github.com/pakkasys/fluidapi/core/api"
+	"github.com/pakkasys/fluidapi/endpoint/middleware"
+)
 
 type EndpointDefinition struct {
 	URL             string
 	Method          string
 	MiddlewareStack middleware.Stack
+}
+
+// EndpointDefinitionsToAPIEndpoints converts a list of endpoint definitions to
+// a list of API endpoints
+//
+//   - endpointDefinitions: A list of endpoint definitions to convert
+func EndpointDefinitionsToAPIEndpoints(
+	endpointDefinitions []EndpointDefinition,
+) []api.Endpoint {
+	endpoints := []api.Endpoint{}
+
+	for _, endpointDefinition := range endpointDefinitions {
+		middlewares := []api.Middleware{}
+		for _, mw := range endpointDefinition.MiddlewareStack {
+			middlewares = append(middlewares, mw.Middleware)
+		}
+
+		endpoints = append(
+			endpoints,
+			api.Endpoint{
+				URL:         endpointDefinition.URL,
+				Method:      endpointDefinition.Method,
+				Middlewares: middlewares,
+			},
+		)
+	}
+
+	return endpoints
+}
+
+// Option is a function that modifies an endpoint definition when it is cloned
+type Option func(*EndpointDefinition)
+
+// CloneEndpointDefinition clones an endpoint definition with options
+func CloneEndpointDefinition(
+	original *EndpointDefinition,
+	options ...Option,
+) *EndpointDefinition {
+	cloned := *original
+	for _, option := range options {
+		option(&cloned)
+	}
+	return &cloned
+}
+
+// WithURL clones an endpoint definition with the provided URL
+func WithURL(url string) Option {
+	return func(e *EndpointDefinition) {
+		e.URL = url
+	}
+}
+
+// WithMethod clones an endpoint definition with the provided HTTP method
+func WithMethod(method string) Option {
+	return func(e *EndpointDefinition) {
+		e.Method = method
+	}
+}
+
+// WithMiddlewareStack clones an endpoint definition with the provided
+// middleware stack.
+func WithMiddlewareStack(
+	stack middleware.Stack,
+) Option {
+	return func(e *EndpointDefinition) {
+		e.MiddlewareStack = stack
+	}
+}
+
+// WithMiddlewareWrappersFunc clones an endpoint definition with the provided
+// middleware wrappers
+func WithMiddlewareWrappersFunc(
+	middlewareWrappersFunc func(
+		endpointDefinition *EndpointDefinition,
+	) middleware.Stack,
+) Option {
+	return func(e *EndpointDefinition) {
+		e.MiddlewareStack = middlewareWrappersFunc(e)
+	}
 }
--- endpoint/definition/definition_test.go
+++ endpoint/definition/definition_test.go
@@ -127,3 +127,97 @@ func TestEndpointDefinitionsToAPIEndpoints_EmptyDefinitions(t *testing.T) {
 	apiEndpoints := EndpointDefinitionsToAPIEndpoints(endpointDefinitions)
 	assert.Len(t, apiEndpoints, 0)
 }
+
+// TestCloneEndpointDefinition tests CloneEndpointDefinition with various
+// options
+func TestCloneEndpointDefinition(t *testing.T) {
+	original := &EndpointDefinition{
+		URL:    "/original-url",
+		Method: "GET",
+		MiddlewareStack: middleware.Stack{
+			{ID: "auth"},
+			{ID: "logging"},
+		},
+	}
+
+	cloned := CloneEndpointDefinition(
+		original,
+		WithURL("/new-url"),
+		WithMethod("POST"),
+		WithMiddlewareStack(
+			middleware.Stack{{ID: "new-middleware"}},
+		),
+	)
+
+	assert.NotSame(t, original, cloned, "cloned object should be new instance")
+	assert.Equal(t, "/new-url", cloned.URL, "URL should be updated")
+	assert.Equal(t, "POST", cloned.Method, "Method should be updated")
+	assert.Equal(
+		t,
+		middleware.Stack{{ID: "new-middleware"}},
+		cloned.MiddlewareStack,
+		"MiddlewareStack should be updated",
+	)
+	assert.Equal(t, "/original-url", original.URL, "URL should not change")
+	assert.Equal(t, "GET", original.Method, "method should not change")
+	assert.Equal(
+		t, middleware.Stack{{ID: "auth"},
+			{ID: "logging"}}, original.MiddlewareStack,
+		"original MiddlewareStack should remain unchanged",
+	)
+}
+
+// TestWithURL tests the WithURL function
+func TestWithURL(t *testing.T) {
+	original := &EndpointDefinition{URL: "/old-url"}
+	option := WithURL("/new-url")
+	option(original)
+
+	assert.Equal(t, "/new-url", original.URL, "URL should be /new-url")
+}
+
+// TestWithMethod tests the WithMethod function
+func TestWithMethod(t *testing.T) {
+	original := &EndpointDefinition{Method: "GET"}
+	option := WithMethod("POST")
+	option(original)
+
+	assert.Equal(t, "POST", original.Method, "Method should be updated to POST")
+}
+
+// TestWithMiddlewareWrappers tests the WithMiddlewareWrappers function
+func TestWithMiddlewareWrappers(t *testing.T) {
+	original := &EndpointDefinition{
+		MiddlewareStack: middleware.Stack{{ID: "auth"}},
+	}
+	newMiddleware := middleware.Stack{{ID: "logging"}}
+	option := WithMiddlewareStack(newMiddleware)
+	option(original)
+
+	assert.Equal(
+		t,
+		newMiddleware,
+		original.MiddlewareStack,
+		"MiddlewareStack should be updated to logging",
+	)
+}
+
+// TestWithMiddlewareWrappersFunc tests the WithMiddlewareWrappersFunc function
+func TestWithMiddlewareWrappersFunc(t *testing.T) {
+	original := &EndpointDefinition{
+		MiddlewareStack: middleware.Stack{{ID: "auth"}},
+	}
+	option := WithMiddlewareWrappersFunc(
+		func(e *EndpointDefinition) middleware.Stack {
+			return middleware.Stack{{ID: "dynamic-middleware"}}
+		},
+	)
+	option(original)
+
+	assert.Equal(
+		t,
+		middleware.Stack{{ID: "dynamic-middleware"}},
+		original.MiddlewareStack,
+		"MiddlewareStack should be dynamically updated",
+	)
+}
